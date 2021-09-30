d1de55aa906bee72c093a455274958c864c9eef3
Xenolithes <evan.schweitzer.hratx44@gmail.com>
2021-09-30 08:24:12 -0400

    TwoNumberSum Go

--- golang/algos/twoNumberSum/twoNumberSum.go
+++ golang/algos/twoNumberSum/twoNumberSum.go
@@ -1,16 +1,13 @@
 package twoNumberSum
 
-import "fmt"
-
 func TwoNumberSum(array []int, target int) []int {
 	// Write your code here.
 	hash := make(map[int]int)
 	for _, num := range array {
 		_, ok := hash[num]
 		if ok {
-			adder := []int{num, target - num}
-			fmt.Println(adder)
-			return adder
+			tuple := []int{num, target - num}
+			return tuple
 		} else {
 			hash[target-num] = num
 		}
