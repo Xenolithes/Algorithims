f2e4f20c1f543df766495a13935243f7b0836164
Xenolithes <evan.schweitzer.hratx44@gmail.com>
2021-10-04 14:31:11 -0400

    conditions python

--- python/tutorials/conditions/conditions.py
+++ python/tutorials/conditions/conditions.py
@@ -2,3 +2,22 @@ age = int(input("how old are you?"))
 
 if age >= 16 and age <= 65:
     print("Have a good day at work")
+
+print("-" * 80)
+
+if age < 16 or age > 65:
+    print("Enjoy your free time")
+else:
+    print("Enjoy work")
+
+
+day = "Saturday"
+temperature = 30
+raining = True
+
+if day == "Saturday" and temperature > 27 or not raining:
+    print("Go Swimming")
+else:
+    print("Learn Python")
+
+
