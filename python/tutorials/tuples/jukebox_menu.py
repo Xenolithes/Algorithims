b64bb4e0062b5f6ebe26c92d805439036f9aa23b
Xenolithes <evan.schweitzer.hratx44@gmail.com>
2021-10-11 19:15:39 -0400

    finished list and tuple section python

--- python/tutorials/tuples/jukebox_menu.py
+++ python/tutorials/tuples/jukebox_menu.py
@@ -1,9 +1,31 @@
 from nested_data import albums
 
-print(albums)
-
+SONGS_LIST_INDEX = 3
+SONG_TITLE_INDEX = 1
 while True:
     print("Select an album (invalid choice exits)")
+    print("0: Exit")
     for index, ( title, artist, year, songs) in enumerate(albums):
-        print("{}: {}, {}, {}, {}".format(index + 1, title, artist, year, songs))
-    break
+        print("{}: {}".format(index + 1, title))
+    
+    choice = int(input())
+    if 1 <= choice <= len(albums):
+        songs_list = albums[choice-1][SONGS_LIST_INDEX]
+    elif choice == 0:
+        break
+    else:
+        print("That is an invalid song choice please try again.")
+        continue
+
+    print("Please Choose your song:")
+    for index, (track_number, song) in enumerate(songs_list):
+        print("{}: {}".format(index+1, song))
+    
+    song_choice = int(input())
+    if  0 < song_choice <= len(songs_list):
+        print("Playing: {}".format(songs_list[song_choice-1][SONG_TITLE_INDEX])
+        )
+    print( "=" * 40)
+
+
+print("Thanks Please Come Again")
